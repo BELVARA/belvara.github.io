bf4b62d348b337eea31cbbfc7d518fdedd6a7160
tora180405 <toradigda@gmail.com>
2023-12-05 16:24:06 +0700

    edit pop up

--- js/script.js
+++ js/script.js
@@ -11,24 +11,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     popupContainer.style.display = "flex";
 
-    // Fungsi untuk menampilkan pop-up
-    window.openPopup = function() {
-        if (popupCount < 3) { // Jumlah pop-up yang diizinkan
-            popupContainer.style.display = "flex";
-            // Ganti konten pop-up sesuai kebutuhan
-            if (popupCount === 0) {
-                // document.body.style.backgroundImage = "url('car3.gif')";
-                // popupText.textContent = "Konten pop-up kedua di sini.";
-            } else{
-                // popupText.textContent = "Konten pop-up ketiga di sini.";
-            }
-            popupCount++;
-        } else {
-            popupContainer.style.display = "none"; // Ganti dengan URL halaman berikutnya
-            // alert("Anda sudah menampilkan pop-up sebanyak 2 kali.");
-        }
-        
+    window.closePopup = function() {
+        document.getElementById("popupContainer").style.display = "none";
     }
+
+    // Fungsi untuk menampilkan pop-up
+    // window.openPopup = function() {
+    //     if (popupCount < 3) { // Jumlah pop-up yang diizinkan
+    //         popupContainer.style.display = "flex";
+    //         // Ganti konten pop-up sesuai kebutuhan
+    //         if (popupCount === 0) {
+    //             // document.body.style.backgroundImage = "url('car3.gif')";
+    //             // popupText.textContent = "Konten pop-up kedua di sini.";
+    //         } else{
+    //             // popupText.textContent = "Konten pop-up ketiga di sini.";
+    //         }
+    //         popupCount++;
+    //     } else {
+    //         popupContainer.style.display = "none"; // Ganti dengan URL halaman berikutnya
+    //         // alert("Anda sudah menampilkan pop-up sebanyak 2 kali.");
+    //     }
+        
+    // }
     // // Fungsi untuk menutup pop-up dan mengarahkan ke halaman berikutnya
     // window.closePopup = function() {
     //     popupContainer.style.display = "none"; // Ganti dengan URL halaman berikutnya
