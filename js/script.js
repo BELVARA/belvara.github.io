1dbd1139df88be633b80c5ad6abde0e723a02d14
tora180405 <toradigda@gmail.com>
2024-01-08 20:29:15 +0700

    edit pop up

--- js/script.js
+++ js/script.js
@@ -1,40 +1,10 @@
-$( function() {
-    $( "#dialog" ).dialog();
-} );
-
-// pop up box
-document.addEventListener("DOMContentLoaded", function() {
-    var popupContainer = document.getElementById("popupContainer");
-    var popupText = document.getElementById("popupText");
-    // var popupimage = document.getElementById("popupimage");
-    var popupCount = 0;
-
-    popupContainer.style.display = "flex";
-
+window.addEventListener("DOMContentLoaded", function() {
+    const popupContainer = document.getElementById("popupContainer");
+    
+    const popupContent = document.querySelector(".popup-content");
+    // popupContent.style.backgroundImage = "url('../img/gif2_rumah_siap_huni.gif')";
+    
     window.closePopup = function() {
         document.getElementById("popupContainer").style.display = "none";
     }
-
-    // Fungsi untuk menampilkan pop-up
-    // window.openPopup = function() {
-    //     if (popupCount < 3) { // Jumlah pop-up yang diizinkan
-    //         popupContainer.style.display = "flex";
-    //         // Ganti konten pop-up sesuai kebutuhan
-    //         if (popupCount === 0) {
-    //             // document.body.style.backgroundImage = "url('car3.gif')";
-    //             // popupText.textContent = "Konten pop-up kedua di sini.";
-    //         } else{
-    //             // popupText.textContent = "Konten pop-up ketiga di sini.";
-    //         }
-    //         popupCount++;
-    //     } else {
-    //         popupContainer.style.display = "none"; // Ganti dengan URL halaman berikutnya
-    //         // alert("Anda sudah menampilkan pop-up sebanyak 2 kali.");
-    //     }
-        
-    // }
-    // // Fungsi untuk menutup pop-up dan mengarahkan ke halaman berikutnya
-    // window.closePopup = function() {
-    //     popupContainer.style.display = "none"; // Ganti dengan URL halaman berikutnya
-    // };
 });
